4f847e575f67a20fe4d1883096b09d6b950af2b3
AugustusTonia <gtonia983@gmail.com>
2022-09-08 14:48:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+init main(void)
+{
+puts ("\"Programming is like bilding a multilingual puzzle" );
+return (0);
+}
