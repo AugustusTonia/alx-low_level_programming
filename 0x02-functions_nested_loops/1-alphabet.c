9750ac21799e0b05d558f607243673a990b50275
AugustusTonia <gtonia983@gmail.com>
2022-09-13 21:23:33 +0100

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints out the stdout
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
+}
