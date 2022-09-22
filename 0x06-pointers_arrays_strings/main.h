ab5439a6060ddf720b4e2b98959373eca3969728
AugustusTonia <gtonia983@gmail.com>
2022-09-22 03:54:45 -0700

    task completed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
