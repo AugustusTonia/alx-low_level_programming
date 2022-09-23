1a432eb318346721d6e4bf595d46680b8f7da209
AugustusTonia <gtonia983@gmail.com>
2022-09-22 17:02:05 -0700

    weldone

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - function that prints an integer
+ * @n: is an integer assigned
+ * Return: nothing
  */
+
 void print_number(int n)
 {
-	unsigned int num = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+unsigned int i;
+if (n < 0)
+{
+_putchar('-');
+i = -n;
+}
+else
+{
+i = n;
+}
+if (i / 10 != 0)
+print_number(i / 10);
+_putchar((i % 10) + '0');
 }
