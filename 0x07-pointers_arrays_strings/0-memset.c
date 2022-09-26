9432568780535a6a94af9e9578697139f139be77
AugustusTonia <gtonia983@gmail.com>
2022-09-26 18:02:27 +0100

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,13 +7,14 @@
 *
 * Return: pointer to memory area s
 */
-char *_memset(char *s, char b, unsigned int n)
 
+char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-*(s + i) = b;
-}
-return (s);
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
