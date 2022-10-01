9f28b240296ab19a2a4f018573af639a7138cebd
AugustusTonia <gtonia983@gmail.com>
2022-09-30 19:11:51 -0700

    work

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * compiled by: Anthonia Akpan
- * Descrp: Header file containing the prototypes of all functions
- *
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -30,3 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
+
