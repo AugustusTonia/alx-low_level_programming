3b712287e03c2541577f4f7e4f22b8e6b360c5e8
AugustusTonia <gtonia983@gmail.com>
2022-10-02 16:15:55 -0700

    good

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -2,6 +2,10 @@
 gcc -c *.c
 ar rc liball.a *.o
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 083a468e4f3cdd7c9a307fe6710557396c299030
+=======
+ranlib liball.a
+>>>>>>> 535f96c5b74d1876cf2e6150d539632dc5efd926
