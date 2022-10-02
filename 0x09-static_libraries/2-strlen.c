0684fcf54863788b5ea36c361f45ef82acf63183
Anthonia Augustus Akpan <111126719+AugustusTonia@users.noreply.github.com>
2022-10-03 00:49:22 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _strlen - checks the length of a string
- * @s: string to check
- *
- * Return: int length of the string
- */
+ * _strlen - Returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+*/
 
 int _strlen(char *s)
 {
 	int len = 0;
 
 	while (*s++)
+	{
 		len++;
+
+	}
+
 	return (len);
 }
