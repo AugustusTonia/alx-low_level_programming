0b14f46feb8673bcaaece558961f3eb971bb955e
AugustusTonia <gtonia983@gmail.com>
2022-10-04 06:36:54 -0700

    main.h file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* File: Main.h file
+* Desc: File containing the prototypes of all the functions used in 0x0B-malloc_free task.
+*
+*/
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+void *malloc(size_t size);
+int _putchar (char);
+
+#endif // MAIN_H
