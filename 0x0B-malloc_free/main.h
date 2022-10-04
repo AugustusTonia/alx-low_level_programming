078e4d94aba57fab64ab8fbe4765883dd08ba368
AugustusTonia <gtonia983@gmail.com>
2022-10-04 13:20:11 -0700

    updated

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -14,7 +14,6 @@ char *str_concat(char *s1, char *s2);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-void *malloc(size_t size);
 int _putchar (char);
 
 #endif // MAIN_H
