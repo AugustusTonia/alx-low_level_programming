2e9608ad055042329f395bfc19d43eafe8d61ee9
AugustusTonia <gtonia983@gmail.com>
2022-10-10 09:21:28 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - function that prints a file name using a predefined 
- * macro
- * Return: always 0 
+ * main - Function that prints the file name
+ * Return: Always 0
  */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
-
